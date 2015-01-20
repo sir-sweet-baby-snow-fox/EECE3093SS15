af51a23c23619e35b5e88ed6c0d002a078e9cc8a
Brian Adams <badams2527@gmail.com>
2015-01-20 16:34:40 -0500

    Add files for project.

---
+++ src/tracing/views/RequirementsIndicesView.java
@@ -0,0 +1,113 @@
+package tracing.views;
+
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.part.ViewPart;
+
+public class RequirementsIndicesView extends ViewPart implements ISelectionProvider{
+	
+	private void showMessage(){
+		MessageDialog.openInformation(new Shell(),
+				"Testhello",
+				"Hello, Eclipse world");
+	}
+	
+	@Override
+	public void addSelectionChangedListener(ISelectionChangedListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ISelection getSelection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void removeSelectionChangedListener(
+			ISelectionChangedListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setSelection(ISelection selection) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		
+		showMessage();
+		
+		//Set layout forum of parent composite
+		parent.setLayout(new FormLayout());
+		
+		FormData formdata = new FormData();
+		formdata.top=new FormAttachment(0,5);
+		formdata.left = new FormAttachment(0,10);
+		formdata.right = new FormAttachment(0,200);
+		
+		//Create title label
+		Label titleLabel = new Label(parent,SWT.SINGLE);
+		titleLabel.setText("Requirements Indices:");
+		titleLabel.setLayoutData(formdata);
+		
+		//Create text area
+		Text indicesText = new Text(parent,SWT.MULTI|SWT.V_SCROLL|SWT.READ_ONLY);
+		indicesText.setText("This is a sample result.");
+		formdata = new FormData();
+		formdata.top = new FormAttachment(titleLabel,10);
+		formdata.bottom = new FormAttachment(titleLabel,230);
+		formdata.left = new FormAttachment(0,10);
+		formdata.right = new FormAttachment(0,800);
+		indicesText.setLayoutData(formdata);
+		
+		
+		Button manageButton = new Button(parent,SWT.PUSH);
+		manageButton.setText("Manage...");
+		formdata = new FormData();
+		formdata.top = new FormAttachment(indicesText,10);
+		formdata.left = new FormAttachment(0,730);
+		manageButton.setLayoutData(formdata);
+		
+		manageButton.addSelectionListener(new SelectionListener(){
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				showMessage();
+				
+			}
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
