0c23af3b162483d41a97ea65e9520e33f3fa2dc1
Brian Adams <badams2527@gmail.com>
2015-01-27 16:31:45 -0500

    #9 Not sure what I changed...

---
+++ src/test/RemoveStopWordsTests.java
@@ -0,0 +1,15 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.nio.file.Files;
+
+import org.junit.Test;
+
+public class RemoveStopWordsTests {
+
+	@Test
+	public void testRemoveOnlyStopWords() {
+	}
+
+}
