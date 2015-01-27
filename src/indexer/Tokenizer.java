8913e3166815a892a6d444c071ceb2f23a222d5e
Brian Adams <badams2527@gmail.com>
2015-01-27 15:46:03 -0500

    #5 Add unit tests.

--- src/indexer/Tokenizer.java
+++ src/indexer/Tokenizer.java
@@ -2,7 +2,7 @@ package indexer;
 
 public class Tokenizer {
 	public String[] TokenizeString(String s) {
-		String[] parts = s.split("[^a-zA-Z\\d\\s\n]");
+		String[] parts = s.split("[^a-zA-Z\\d]+");
 		return parts;
 	}
 }
